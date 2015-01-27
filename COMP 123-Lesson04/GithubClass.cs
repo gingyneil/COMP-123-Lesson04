9d7f4b29e616e3ae50dd5179da1aa6eb88dede14
gingyneil <neil.reading.nr@gmail.com>
2015-01-27 12:47:49 -0500

    Initial Commit

---
+++ COMP 123-Lesson04/GithubClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP_123_Lesson04
+{
+    class GithubClass
+    {
+    //GitHub Test
+    }
+}
